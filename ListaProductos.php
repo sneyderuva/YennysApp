d350e7bc9c3ce69677f02e3083c112867b4170b2
sneyderuva <87355886+sneyderuva@users.noreply.github.com>
2022-07-02 18:02:03 -0500

    Update ListaProductos.php

--- ListaProductos.php
+++ ListaProductos.php
@@ -6,11 +6,11 @@ if($resultset=getSQLResultSet("SELECT * FROM productos")){
 	
 	while ($row = $resultset->fetch_array(MYSQLI_NUM)){
 		$e = array();
-                $e['id'] = $row[0];
-                $e['nombreproducto'] = $row[1];
-                $e['descripcionproducto'] = $row[2];
-                $e['fotoproducto'] = $row[3];
-                $e['precioproducto'] = $row[4];
+                $e['Id'] = $row[0];
+                $e['Nombre'] = $row[1];
+                $e['Foto'] = $row[2];
+                $e['Precio'] = $row[3];
+                
                 array_push($array,$e);
 	}
         echo json_encode($array);
